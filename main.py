59aa4a9920c6d03a855abcb65cf8b50fba3936e6
AlexisGuanique <guaniqued@gmail.com>
2025-01-29 19:18:19 +0000

    ajustes rapidos

--- main.py
+++ main.py
@@ -14,34 +14,28 @@ def on_login_success(login_data):
 
     if name and lastname:
         full_name = f"{name} {lastname}"
-        # print(f"Bienvenido, {full_name}")
         setup_ui(full_name, on_login_success)
     else:
         print("Error: No se pudo obtener el nombre o apellido del usuario")
 
-
 def main():
     create_database()
 
-    # Consultar si hay un usuario logueado
     logged_in_user = get_logged_in_user()
 
     if logged_in_user:
-        # Verificar el token antes de continuar
-        # Si retorna None, asigna {"is_valid": False}
+
         token_status = verify_token() or {"is_valid": False}
 
         if not token_status.get("is_valid"):
-            # Si el token es inválido o ha expirado, mostrar mensaje y forzar logout
             messagebox.showerror(
                 "Token Expirado", "Usuario expirado. Contacte a los desarrolladores.")
             logout()
             setup_auth_ui(on_login_success)
             return
 
-        # Si el token es válido, preguntar si quiere continuar logueado
         root = tk.Tk()
-        root.withdraw()  # Ocultar la ventana principal temporalmente
+        root.withdraw() 
 
         name = logged_in_user.get("name")
         lastname = logged_in_user.get("lastname")
@@ -54,17 +48,14 @@ def main():
         )
 
         if result:
-            # print(f"Usuario {full_name} continúa logueado.")
             root.destroy()
             setup_ui(full_name, on_login_success)
         else:
-            # Si el usuario no desea continuar logueado
             logout()
             messagebox.showinfo("Logout Exitoso", "Has cerrado sesión.")
             root.destroy()
             setup_auth_ui(on_login_success)
     else:
-        # Si no hay un usuario logueado, mostrar la pantalla de login
         setup_auth_ui(on_login_success)
 
 
